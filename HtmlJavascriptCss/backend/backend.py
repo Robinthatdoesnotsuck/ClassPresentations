63499fe4e0a1a5e59e2601900854b98dbc6fac14
Robinthatdoesnotsuck <iram644@hotmail.com>
2026-02-05 18:44:01 -0600

    basic form handling

--- HtmlJavascriptCss/backend/backend.py
+++ HtmlJavascriptCss/backend/backend.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 
 port = "6666"
@@ -9,6 +9,7 @@ app = Flask(__name__)
 CORS(app)
 
 
+
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
@@ -17,3 +18,21 @@ def hello_world():
 @app.route("/users")
 def hello_users():
     return "<p>Hello, World!</p>"
+
+@app.route("/process", methods = ["POST"])
+def process_data():
+    data = request.get_json()
+
+    if not data:
+        return jsonify({"error": "No body in message"}), 400
+    
+    name = data.get('name','Stranger')
+
+    response = {
+        "status": "ok",
+        "message": f"I've saved the user:{name}"
+    }
+
+    return jsonify(response), 200
+
+" Recibo cosas, proceso cosas, guardo cosas y o regreso cosas"
